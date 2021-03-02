b7e03f8675ed5daeb012f1888683795a777a0493
Kevin Jovel <kevinjovel9@gmail.com>
2021-03-02 14:53:45 -0600

    configuracion de material angular

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesHomeComponent } from './pages/heroes-home/heroes-home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+
 
 
 
@@ -18,6 +24,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     ListadoComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
